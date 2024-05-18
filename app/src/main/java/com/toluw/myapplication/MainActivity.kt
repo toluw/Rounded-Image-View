4f0f01333ba80b7efa90c70a4068be4abf9fa372
toluw <oketoluwase@gmail.com>
2024-05-18 12:22:39 +0100

    initial comit

---
+++ app/src/main/java/com/toluw/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.toluw.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toluw.circularimageview.RoundedImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
