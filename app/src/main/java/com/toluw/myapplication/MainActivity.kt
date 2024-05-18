b68cb2bddfd4091c6648ee0df44436f8d4e452a7
toluw <oketoluwase@gmail.com>
2024-05-18 13:15:58 +0100

    Upgrade Java version

--- app/src/main/java/com/toluw/myapplication/MainActivity.kt
+++ app/src/main/java/com/toluw/myapplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.toluw.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.toluw.circularimageview.RoundedImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
